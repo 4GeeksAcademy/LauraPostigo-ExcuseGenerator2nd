c2d56c1746d0f70510d8e38daf5994f15fc0674b
LauraPostigo <166739141+LauraPostigo@users.noreply.github.com>
2024-11-21 09:57:11 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,26 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let who = ["The dog", "My grandma", "The mailman", "My bird"];
+let action = ["ate", "peed on", "crushed", "broke"];
+let what = ["my homework", "my phone", "the car", "my keys"];
+let when = [
+  "before the class",
+  "when I was sleeping",
+  "while I was exercising",
+  "during my lunch",
+  "while I was praying"
+];
+
+function generateExcuse() {
+  // Selecciona un elemento aleatorio de cada array
+  let whoPart = who[Math.floor(Math.random() * who.length)];
+  let actionPart = action[Math.floor(Math.random() * action.length)];
+  let whatPart = what[Math.floor(Math.random() * what.length)];
+  let whenPart = when[Math.floor(Math.random() * when.length)];
+  return `${whoPart} ${actionPart} ${whatPart} ${whenPart}.`;
+}
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.getElementById("excuse").innerHTML = generateExcuse();
 };
